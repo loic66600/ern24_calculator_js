59f5adb5fcb5d055b006b2d8a051e591b2b3e91a
loic <loic.rossignol66@gmail.com>
2024-04-04 16:36:36 +0200

    add calculat ok

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello")
+//fonction pour ajouter un élément a l ecran
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+//fonction pour efffacer l ecran
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+// calculer
+function calculate() {
+    //dans un variable on va stocker la valeur de l input
+    let expression = document.getElementById('display').value;
+    console.log(expression);
+    let result = eval(expression);
+    console.log(result)
+    document.getElementById('display').value = result;
+}
